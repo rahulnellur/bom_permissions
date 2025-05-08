efc5aefc256ea994bd16f77fe510cf9f2639d9dd
Rahul Kv <rahulkv@Mac-mini.local>
2025-05-08 20:54:26 +0530

    Fix syntax error in setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='bom_permissions',
     version='0.0.1',
-<<<<<<< HEAD
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[],
-)
-=======
-    description='Custom BOM Permission Manager for ERPNext',
-    author='Rahul Kv',
-    author_email='your.email@example.com',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[],
 )
 
->>>>>>> d6e6577fb4ab1a4158202286189698970accd9d6
